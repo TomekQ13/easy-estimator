0278b723ce55b7d414ad7b4adc196213d9d681ae
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-03-13 11:07:02 +0100

    beginning of tests

---
+++ api/app/models/session.js
@@ -0,0 +1,31 @@
+const client = require('../db.js')
+
+async function getSession(sessionId) {
+    let resp = await client.query(`
+        select host_id, session_id, params
+        from estimation_session
+        where session_id = $1
+    `, [sessionId])
+    if (resp.rows[0]) return resp.rows[0]
+    return undefined
+}
+
+async function createNewSession(sessionId, hostId, params) {
+    await client.query(`
+        insert into estimation_session (host_id, session_id, params)
+        values ($1, $2, $3)
+        
+    `, [hostId, sessionId, params])
+}
+
+async function updateSession(sessionId, params) {
+    await client.query(`
+        update estimation_session
+        set params = $1
+        where sessionId = $2
+    `, [params, sessionId])
+}
+
+
+
+module.exports = { getSession, createNewSession, updateSession }
