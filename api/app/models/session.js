533c932cec1bdbb101740924fd3d4b1a28ade0d9
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-03-13 16:00:21 +0100

    session model tests completed

--- api/app/models/session.js
+++ api/app/models/session.js
@@ -2,30 +2,42 @@ const client = require('../db.js')
 
 async function getSession(sessionId) {
     let resp = await client.query(`
-        select host_id, session_id, params
+        select
+            host_id,
+            session_id,
+            params as params
         from estimation_session
         where session_id = $1
     `, [sessionId])
-    if (resp.rows[0]) return resp.rows[0]
+    if (resp.rows.length > 0) return resp.rows[0]
     return undefined
 }
 
 async function createNewSession(sessionId, hostId, params) {
-    await client.query(`
+    let resp = await client.query(`
         insert into estimation_session (host_id, session_id, params)
-        values ($1, $2, $3)
-        
+        values ($1, $2, $3)        
     `, [hostId, sessionId, params])
+    return resp
 }
 
 async function updateSession(sessionId, params) {
-    await client.query(`
+    let resp = await client.query(`
         update estimation_session
         set params = $1
-        where sessionId = $2
+        where session_id = $2
     `, [params, sessionId])
+    return resp
+}
+
+async function deleteSession(sessionId) {
+    let resp = await client.query(`
+        delete from estimation_session
+        where session_id = $1
+    `, [sessionId])
+    return resp
 }
 
 
 
-module.exports = { getSession, createNewSession, updateSession }
+module.exports = { getSession, createNewSession, updateSession, deleteSession }
