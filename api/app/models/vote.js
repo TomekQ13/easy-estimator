c5878ea84bcb536249e89b4b2fe7b3824d2788bd
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-03-15 23:21:13 +0100

    tests for votes are in progress

--- api/app/models/vote.js
+++ api/app/models/vote.js
@@ -11,18 +11,28 @@ async function getVotes(sessionId) {
 }
 
 async function vote(sessionId, voteId, userId, voteValue) {
-    await client.query(`
+    let resp = await client.query(`
         insert into votes (vote_id, session_id, user_id, vote_value)
-        values ($1, $2, $3)
+        values ($1, $2, $3, $4)
     `, [voteId, sessionId, userId, voteValue])
+    return resp
 }
 
 async function updateVote(sessionId, voteId, userId, voteValue) {
-    await client.query(`
+    let resp = await client.query(`
         update votes
         set vote_value = $4
         where sessionId = $1, voteId = $2, userId = $3
     `, [sessionId, voteId, userId, voteValue])
+    return resp
 }
 
-module.exports = { getVotes, vote, updateVote }
+async function deleteVote(sessionId, voteId, userId, voteValue) {
+    let resp = await client.query(`
+        delete from votes
+        where sessionId = $1, voteId = $2, userId = $3
+    `, [sessionId, voteId, userId, voteValue])
+    return resp
+}
+
+module.exports = { getVotes, vote, updateVote, deleteVote }
---
+++ api/test/models/vote.js
@@ -0,0 +1,68 @@
+const { getVotes, vote, updateVote, deleteVote } = require('../../app/models/vote')
+const expect = require('chai').expect
+
+const TESTING_VOTE1 = {
+    sessionId: 'sessionId',
+    voteId: 'voteId',
+    userId: 'userId',
+    voteValue: 3
+}
+const TESTING_VOTE2 = {
+    sessionId: 'sessionId2',
+    voteId: 'voteId2',
+    userId: 'userId2',
+    voteValue: 2
+}
+const TESTING_VOTE3 = {
+    sessionId: 'sessionId',
+    voteId: 'voteId3',
+    userId: 'userId2',
+    voteValue: 1
+}
+
+describe('Vote interface functions', () => {
+    describe('create new votes', () => {
+        it('create new vote 1', async () => {
+            let resp = await vote(TESTING_VOTE1.sessionId, TESTING_VOTE1.voteId, TESTING_VOTE1.userId, TESTING_VOTE1.voteValue)
+            expect(resp.rowCount).to.equal(1)
+        })
+        it('create new vote 2', async () => {
+            let resp = await vote(TESTING_VOTE2.sessionId, TESTING_VOTE2.voteId, TESTING_VOTE2.userId, TESTING_VOTE2.voteValue)
+            expect(resp.rowCount).to.equal(1)
+        })
+        it('create new vote 3', async () => {
+            let resp = await vote(TESTING_VOTE3.sessionId, TESTING_VOTE3.voteId, TESTING_VOTE3.userId, TESTING_VOTE3.voteValue)
+            expect(resp.rowCount).to.equal(1)
+        })
+    })
+
+    describe('get multiple votes', () => {
+        it('get votes', async () => {
+            let resp = await getVotes(TESTING_VOTE1.sessionId)
+            expect(resp.length).to.equal(2)
+        })
+    })
+
+    TESTING_VOTE2.voteValue = 4
+
+    describe('update vote', () => {
+        it('update vote', async () => {
+            let resp = await updateVote(TESTING_VOTE2.sessionId, TESTING_VOTE2.voteId, TESTING_VOTE2.userId, TESTING_VOTE2.voteValue)
+            expect(resp.rowCount).to.equal(1)
+        })
+        it('get updated vote', () => {
+            let resp = await getVotes(TESTING_VOTE2)
+            expect(resp.length).to.equal(1)
+            expect(resp[0].vote_id).to.equal(TESTING_VOTE2.voteId)
+            expect(resp[0].session_id).to.equal(TESTING_VOTE2.sessionId)
+            expect(resp[0].user_id).to.equal(TESTING_VOTE2.userId)
+            expect(resp[0].vote_value).to.equal(TESTING_VOTE2.voteValue)
+        })
+    })
+
+    describe('delete votes', () => {
+        it('delete vote 1', async () => {
+            let resp = deleteVote()
+        })
+    })
+})
