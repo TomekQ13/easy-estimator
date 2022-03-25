855ec63cfcc2d87ce8acfc1b3d5fdad14b2f9074
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-03-25 23:11:14 +0100

    setting up routing

--- api/test/routes/session.js
+++ api/test/routes/session.js
@@ -1,5 +1,12 @@
 const { expect } = require("chai")
 const request = require("request")
+const chai = require('chai')
+// Import the dependencies for testing
+const chaiHttp = require('chai-http')
+const app = require( '../../app/app')
+// Configure chai
+chai.use(chaiHttp);
+chai.should()
 
 const URL = 'http://localhost:4000'
 
@@ -13,22 +20,36 @@ describe('Session API endpoints', () => {
     })
 
     describe('Create session' ,() => {
-        it('201 response code', () => {
-            request.post({
-                url: `${URL}/session/test_session_id`,
-                form: { hostId: 'test_host', params: {a: 1, b:'test_params'} }
-            }, (_error, response) => {
-                expect(response.statusCode).to.equal(201)
-            })
+        it('201 response code', (done) => {
+            chai.request(app)
+                .post(`${URL}/session/test_session_id`,)
+                .type('form')
+                .send({
+                    hostId: 'test_host',
+                    params: {a: 1, b:'test_params'}
+                })
+                .end((_err, res) => {
+                    // res.should.have.status(201)
+                    expect(res.statusCode).to.equal(201)
+                    done()
+                })
+
+            // request({
+            //     method: 'post',
+            //     url: `${URL}/session/test_session_id`,
+            //     body: { hostId: 'test_host', params: {a: 1, b:'test_params'} }
+            // }, (_error, response) => {
+            //     expect(response.statusCode).to.equal(201)
+            // })
         })
     })
 
 
-    describe('Get existing session', () => {
-        it('200 response code', () => {
-            request(`${URL}/session/test_session_id`, (_error, response) => {
-                expect(response.statusCode).to.equal(200)
-            })
-        })
-    })
+    // describe('Get existing session', () => {
+    //     it('200 response code', () => {
+    //         request(`${URL}/session/test_session_id`, (_error, response) => {
+    //             expect(response.statusCode).to.equal(200)
+    //         })
+    //     })
+    // })
 })
--- react-app/src/components/App.js
+++ react-app/src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "../css/app.css"
 import EntryMenu from "./EntryMenu";
+import {Route, Routes, BrowserRouter as Router} from 'react-router-dom'
 
 
 function App() {
   return (
-   <EntryMenu />
+    <Router>
+        <Routes>
+            <Route path='/' exact element={ <EntryMenu/>} />
+        </Routes>        
+    </Router>
   )
 }
 
