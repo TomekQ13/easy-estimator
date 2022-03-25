523edfaf7d2fe2abdb63d0421b72e0529db2d986
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-03-25 23:47:47 +0100

    extracting variables from path

--- react-app/src/components/App.js
+++ react-app/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../css/app.css"
 import EntryMenu from "./EntryMenu";
+import Session from "./Session";
 import {Route, Routes, BrowserRouter as Router} from 'react-router-dom'
 
 
@@ -9,6 +10,7 @@ function App() {
     <Router>
         <Routes>
             <Route path='/' exact element={ <EntryMenu/>} />
+            <Route path='/session/:session_id' element={ <Session />} />
         </Routes>        
     </Router>
   )
