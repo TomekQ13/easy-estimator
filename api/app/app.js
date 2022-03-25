94353734fc640415332e30dadc27cfbd6f77aed4
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-03-25 23:11:14 +0100

    setting up routing

--- api/app/app.js
+++ api/app/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const voteRouter = require('./routes/vote')
@@ -13,4 +15,4 @@ app.listen(process.env.PORT || 4000, () => {
     console.log("Application started and Listening on port 4000");
   });
   
-  
+module.exports = app
