65cb5ed02878490c66fea121d2f282d03926e18b
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-03-29 22:24:16 +0200

    endpoint tests finally work :)

--- api/test/routes/session.js
+++ api/test/routes/session.js
@@ -22,10 +22,11 @@ describe('Session API endpoints', () => {
     describe('Create session' ,() => {
         it('201 response code', (done) => {
             chai.request(app)
-                .post(`${URL}/session/test_session_id`,)
+                .post(`/session/test_session_id`,)
                 .type('form')
                 .send({
                     hostId: 'test_host',
+                    sessionPassword: '',
                     params: {a: 1, b:'test_params'}
                 })
                 .end((_err, res) => {
@@ -33,14 +34,6 @@ describe('Session API endpoints', () => {
                     expect(res.statusCode).to.equal(201)
                     done()
                 })
-
-            // request({
-            //     method: 'post',
-            //     url: `${URL}/session/test_session_id`,
-            //     body: { hostId: 'test_host', params: {a: 1, b:'test_params'} }
-            // }, (_error, response) => {
-            //     expect(response.statusCode).to.equal(201)
-            // })
         })
     })
 
