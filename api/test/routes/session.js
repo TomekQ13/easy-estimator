3027441a373edc6c98969c7cb686bc80baeae903
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-03-31 22:54:42 +0200

    unit and int test

--- api/test/routes/session.js
+++ api/test/routes/session.js
@@ -1,6 +1,6 @@
-const { expect } = require("chai")
 const request = require("request")
 const chai = require('chai')
+const expect = chai.expect
 // Import the dependencies for testing
 const chaiHttp = require('chai-http')
 const app = require( '../../app/app')
@@ -9,6 +9,12 @@ chai.use(chaiHttp);
 chai.should()
 
 const URL = 'http://localhost:4000'
+const TESTING_SESSION = {
+    sessionId: 'testSessionIntegration',
+    hostId: 'testHostIntegration',
+    sessionPassword: 'testPasswordIntegration',
+    params: {a: '1', b:'test param', c: ['1','2','3','4','5'], d: { aa:'1', bb:'2', c:['abc', 'dedasdasd'] }}
+}
 
 describe('Session API endpoints', () => {
     describe('Select not existing session' ,() => {
@@ -20,29 +26,46 @@ describe('Session API endpoints', () => {
     })
 
     describe('Create session' ,() => {
+        it('deleting the session if it exists from previous runs', (done) => {
+            chai.request(app)
+            .delete(`/session/${TESTING_SESSION.sessionId}`)
+            .end((_err, res) => {
+                expect(res.status).to.be.oneOf([200, 404])
+                done()
+            })
+        })
+
+
         it('201 response code', (done) => {
             chai.request(app)
-                .post(`/session/test_session_id`,)
-                .type('form')
-                .send({
-                    hostId: 'test_host',
-                    sessionPassword: '',
-                    params: {a: 1, b:'test_params'}
-                })
-                .end((_err, res) => {
-                    // res.should.have.status(201)
-                    expect(res.statusCode).to.equal(201)
-                    done()
-                })
+            .post(`/session/${TESTING_SESSION.sessionId}`)
+            .type('form')
+            .send({
+                hostId: TESTING_SESSION.hostId,
+                sessionPassword: TESTING_SESSION.sessionPassword,
+                params: TESTING_SESSION.params
+            })
+            .end((_err, res) => {
+                expect(res).to.have.status(201)                
+                done()
+
+            })
         })
     })
 
+    describe('Get existing session', () => {
+        it('fetched session is equal to created session', (done) => {    
+            chai.request(app)
+            .get(`/session/${TESTING_SESSION.sessionId}`)
+            .end((err, res) => {
+                expect(res).to.have.status(200)
+                expect(res.body.sessionid).to.equal(TESTING_SESSION.sessionId)
+                expect(res.body.hostid).to.equal(TESTING_SESSION.hostId)
+                expect(res.body.params).to.deep.equal(TESTING_SESSION.params)
+                expect(res.body.password).to.equal(TESTING_SESSION.sessionPassword)
+                done()
+            })
+        })   
 
-    // describe('Get existing session', () => {
-    //     it('200 response code', () => {
-    //         request(`${URL}/session/test_session_id`, (_error, response) => {
-    //             expect(response.statusCode).to.equal(200)
-    //         })
-    //     })
-    // })
+    })
 })
