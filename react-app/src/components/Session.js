59e0fde5654f001c6488072b84d4e31f8ccb2c55
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-04-02 23:47:43 +0200

    added votes qeyr

--- react-app/src/components/Session.js
+++ react-app/src/components/Session.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
+import config from './../config.json'
 
 export default function Session() {
     const sessionId = useParams().session_id
     const [sessionData, setSessionData] = useState({})
+    const [votes, setVotes] = useState([])
 
     useEffect(() => {
         getSession(sessionId)
@@ -14,9 +16,19 @@ export default function Session() {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' }
         }
-        const selectedSession = await fetch(`http://localhost:4000/session/${sessionId}`, requestOptions)
-        const jsonData = await selectedSession.json()
-        setSessionData(jsonData)
+        const selectedSession = await fetch(`${config.apiUrl}/session/${sessionId}`, requestOptions)
+        const sessionJsonData = await selectedSession.json()
+        setSessionData(sessionJsonData)
+    }
+
+    async function getVotes(sessionId) {
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        }
+        const selectedVotes = await fetch(`${config.apiUrl}/vote/${sessionId}`, requestOptions)
+        const votesJsonData = await selectedVotes.json()
+        setVotes(votesJsonData)
     }
 
     return (
@@ -24,6 +36,7 @@ export default function Session() {
             <p>{sessionData.sessionid}</p>
             <p>{sessionData.hostid}</p>
             <p>{sessionData.password}</p>
+            <p>{votes}</p>
         </div>
     )
 }
