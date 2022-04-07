be96115ca4d29a38c2ed44fdc9540889f67bb7d3
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-04-07 22:12:05 +0200

    jwt authentication start

--- api/app/routes/user.js
+++ api/app/routes/user.js
@@ -1,23 +1,47 @@
 const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
+const { createUser, deleteUser } = require('../models/user')
 
-router.post('register', (req, res) => {
-
-})
-
-router.post('login', (req, res) => {
-    // Authenticate the user
-    const userId = 'dummyId' // this should come from db
-
+router.post('/register', async (req, res) => {
+    try {
+        await createUser(req.body.userId, req.body.username)
+    } catch (e) {
+        console.error(e)
+        // res.status(500).json({message: 'There has been an error. Please try again.'})
+    }
     const user = {
         username: req.body.username,
-        userId: userId
+        userId: req.body.userId
     }
-
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken })
-
+    return res.status(201).json({
+        message: `User registered with id ${req.body.userId} and username ${req.body.username}`,
+        accessToken: accessToken
+    })
 })
+
+router.delete('/delete', async (req, res) => {
+    try {
+        await deleteUser(req.body.userId)
+    } catch (e) {
+        console.error(e)
+        
+    }
+})
+
+// router.post('login', (req, res) => {
+//     // Authenticate the user
+//     const userId = 'dummyId' // this should come from db
+
+//     const user = {
+//         username: req.body.username,
+//         userId: userId
+//     }
+
+//     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+//     res.json({ accessToken: accessToken })
+
+// })
 
 module.exports = router
