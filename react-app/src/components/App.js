8ffe7634d50fbf18a810e72e959c31b7d9b29feb
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-07-26 23:08:19 +0200

    showing modal for unregistered users

--- react-app/src/components/App.js
+++ react-app/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import "../css/app.css"
 import EntryMenu from "./EntryMenu";
 import {Session} from "./Session";
@@ -8,14 +8,16 @@ import { getUsernameFromLS } from '../contexts/Auth';
 
 function App() {
 
+
     const username = getUsernameFromLS()
+    
 
     return (
         <Router>
             <Auth>
                 <Routes>
-                        <Route path='/' exact element={ <EntryMenu username={username} />} />
-                        <Route path='/session/:session_id' element={ <Session username={username}/>} />
+                        <Route path='/' exact element={ <EntryMenu username={username} registerModal={registerModal} setRegisterModal={setRegisterModal} />} />
+                        <Route path='/session/:session_id' element={ <Session username={username} registerModal={registerModal} setRegisterModal={setRegisterModal}/>} />
                 </Routes>
             </Auth>     
         </Router>
