934ec1b3d75dc5d563292823fad6eecfd5abfa7d
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-09-19 00:07:12 +0200

    voting works

--- react-app/src/components/ResetVotesBtn.js
+++ react-app/src/components/ResetVotesBtn.js
@@ -4,15 +4,18 @@ import { authContext } from '../contexts/Auth'
 import { SessionContext } from './Session'
 
 
-export default function ResetVotesBtn({ setVotes }) {
+export default function ResetVotesBtn({ setVotes, websocket }) {
     const { accessToken, refreshToken, setAccessToken } = useContext(authContext)
     const { sessionData } = useContext(SessionContext)
 
     // this should be handled as just removing the votes components
     async function handleClick() {
         try {
-            deleteVotes({ sessionId: sessionData.sessionid, accessToken, refreshToken, setAccessTokenFunction: setAccessToken })
+            websocket.send(JSON.stringify({
+                type: 'resetVoting'
+            }))
             setVotes([])
+            deleteVotes({ sessionId: sessionData.sessionid, accessToken, refreshToken, setAccessTokenFunction: setAccessToken })
         } catch (e) {
             console.error(e)
             console.error('There has been an error while deleting votes')
