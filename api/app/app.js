6f3b9167d288c824f168b8733f50fcb50fbb3240
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-10-30 17:31:37 +0100

    deployment progressed

--- api/app/app.js
+++ api/app/app.js
@@ -1,32 +1,32 @@
-require('dotenv').config({ path: './app/.env' })
 const express = require("express");
-const expressWinston = require('express-winston')
+const expressWinston = require("express-winston");
 
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
-
-const { requestLogger } = require('./logger')
+const { requestLogger } = require("./logger");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
-app.use(expressWinston.logger({
-    winstonInstance: requestLogger,
-    statusLevels: true
-}))
+app.use(
+    expressWinston.logger({
+        winstonInstance: requestLogger,
+        statusLevels: true,
+    })
+);
 
-const voteRouter = require('./routes/vote')
-app.use('/vote', voteRouter)
-const sessionRouter = require('./routes/session')
-app.use('/session', sessionRouter)
-const userRotuer = require('./routes/user')
-app.use('/user', userRotuer)
+const voteRouter = require("./routes/vote");
+app.use("/vote", voteRouter);
+const sessionRouter = require("./routes/session");
+app.use("/session", sessionRouter);
+const userRotuer = require("./routes/user");
+app.use("/user", userRotuer);
 
 app.listen(process.env.PORT || 4000, () => {
     console.log("Application started and Listening on port 4000");
-  });
-  
-module.exports = app
+});
+
+module.exports = app;
--- api/app/db.js
+++ api/app/db.js
@@ -1,16 +1,17 @@
-require('dotenv').config()
-const { Client } = require('pg')
+const { Client } = require("pg");
 
 const client = new Client({
     user: process.env.POSTGRES_RW_USER,
     host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_RW_PASSWORD,
-    port: process.env.POSTGRES_PORT
-})
+    port: process.env.POSTGRES_PORT,
+});
 
-client.connect()
+client.connect();
 
-client.query('select now()').then(res => console.log('Connected to database ' + res.rows[0].now))
+client
+    .query("select now()")
+    .then((res) => console.log("Connected to database " + res.rows[0].now));
 
-module.exports = client
+module.exports = client;
