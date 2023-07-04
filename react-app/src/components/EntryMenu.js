beb025b14e74cadd0890284afc4c1cbc3847cc26
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2023-07-04 22:47:01 +0200

    removed new session modal

--- react-app/src/components/EntryMenu.js
+++ react-app/src/components/EntryMenu.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import JoinSessionBtn from "./JoinSessionBtn";
 import NewSessionBtn from "./NewSessionBtn";
-import NewSessionModal from "./NewSessionModal";
 import JoinSessionModal from "./JoinSessionModal";
 import { authContext } from "../contexts/Auth";
 import Container from "react-bootstrap/esm/Container";
@@ -9,7 +8,6 @@ import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
 
 export default function EntryMeny() {
-    const [newSessionModal, setNewSessionModal] = useState({ show: false });
     const [joinSessionModal, setJoinSessionModal] = useState({ show: false });
 
     const { username, setRegisterModal, userId } = useContext(authContext);
@@ -30,10 +28,7 @@ export default function EntryMeny() {
             <Container className="vh-75 d-flex align-items-center justify-content-center">
                 <Col className="wid-500 ">
                     <Row className="mb-3">
-                        <NewSessionBtn
-                            setNewSessionModal={setNewSessionModal}
-                            newSessionModal={newSessionModal}
-                        />
+                        <NewSessionBtn />
                     </Row>
                     <Row>
                         <JoinSessionBtn
