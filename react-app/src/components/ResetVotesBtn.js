b418fedd7243870cd61b5c7dd307b283eea34341
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2023-08-03 22:49:09 +0200

    added error notification

--- react-app/src/components/ResetVotesBtn.js
+++ react-app/src/components/ResetVotesBtn.js
@@ -3,35 +3,44 @@ import { useDeleteVotes } from "../models/vote";
 import { SessionContext } from "./Session";
 import Button from "react-bootstrap/Button";
 import { useUpdateSession } from "../models/session";
+import uuid from "react-uuid";
 
-export default function ResetVotesBtn({ websocket, setShowVotes }) {
+export default function ResetVotesBtn({ setShowVotes }) {
     const [deleteVotes, _resp] = useDeleteVotes();
-    const { sessionId, users, setUsers } = useContext(SessionContext);
+    const { sessionId, users, setUsers, ws, addMessage } =
+        useContext(SessionContext);
     const [updateSession, __resp] = useUpdateSession();
 
     // this should be handled as just removing the votes components
     async function handleClick() {
-        websocket.send(
-            JSON.stringify({
+        try {
+            ws.sendMessage({
                 type: "resetVoting",
                 sessionId,
-            })
-        );
-        const newUsers = [...users];
-        newUsers.map((user) => {
-            user.votevalue = null;
-        });
-        setUsers(newUsers);
+            });
+            const newUsers = [...users];
+            newUsers.map((user) => {
+                user.votevalue = null;
+            });
+            setUsers(newUsers);
 
-        deleteVotes({
-            sessionId,
-        });
+            deleteVotes({
+                sessionId,
+            });
 
-        setShowVotes(false);
-        updateSession({
-            sessionId,
-            newParam: { showVotes: false },
-        });
+            setShowVotes(false);
+            updateSession({
+                sessionId,
+                newParam: { showVotes: false },
+            });
+        } catch (e) {
+            const newMessage = {
+                text: `Connection lost. Please refresh the page.`,
+                type: "error",
+                id: uuid(),
+            };
+            addMessage({ newMessage });
+        }
     }
 
     return (
--- react-app/src/components/ShowVotesBtn.js
+++ react-app/src/components/ShowVotesBtn.js
@@ -2,34 +2,31 @@ import React, { useContext } from "react";
 import Button from "react-bootstrap/Button";
 import { useUpdateSession } from "../models/session";
 import { SessionContext } from "./Session";
+import uuid from "react-uuid";
 
-export default function ShowVotesBtn({ websocket }) {
-    const { sessionId, setShowVotes } = useContext(SessionContext);
+export default function ShowVotesBtn() {
+    const { sessionId, setShowVotes, ws, addMessage } =
+        useContext(SessionContext);
     const [updateSession, _resp] = useUpdateSession();
 
     async function handleClick() {
         try {
-            websocket.send(
-                JSON.stringify({
-                    type: "showVotes",
-                    sessionId,
-                })
-            );
-        } catch (e) {
-            console.error(e);
-            console.error(
-                "There has been an error when sending websocket message to show votes"
-            );
-        }
-        try {
+            ws.sendMessage({
+                type: "showVotes",
+                sessionId,
+            });
             setShowVotes(true);
             updateSession({
                 sessionId,
                 newParam: { showVotes: true },
             });
         } catch (e) {
-            console.error(e);
-            console.error("There has been an error when updating session data");
+            const newMessage = {
+                text: `Connection lost. Please refresh the page.`,
+                type: "error",
+                id: uuid(),
+            };
+            addMessage({ newMessage });
         }
     }
 
