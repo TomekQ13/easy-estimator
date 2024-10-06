68ca41cf68b0cc212be03cd86ad9570908576e58
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2024-10-06 20:21:56 +0200

    adjusted config

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(agilepoker.fun www.agilepoker.fun)
 rsa_key_size=4096
-data_path="./certbot/www"
+data_path="./nginx/ssl"
 email="kuczak.tomasz@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
