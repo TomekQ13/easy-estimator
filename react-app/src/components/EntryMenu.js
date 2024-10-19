b45a238cdbd2ab73195fd67ce7b114abf25d57d1
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2024-10-19 16:16:35 +0200

    added robots.txt

--- react-app/src/components/EntryMenu.js
+++ react-app/src/components/EntryMenu.js
@@ -31,14 +31,17 @@ export default function EntryMeny() {
                     <div className="how-to-section-steps">
                         <StepCard
                             pictureSource={Step1GIF}
+                            alt="How to start instruction step 1. Start a new estimation session by click New session button"
                             text="Start a new session"
                         />
                         <StepCard
                             pictureSource={Step2GIF}
+                            alt="How to start instruction step 2. Copy the estimation session URL and send it to your team"
                             text="Send the URL to your team"
                         />
                         <StepCard
                             pictureSource={Step3GIF}
+                            alt="How to start instruction step 3. Vote and discuss the estimations"
                             text="Vote and discuss your estimates"
                         />
                     </div>
--- react-app/src/components/StepCard.js
+++ react-app/src/components/StepCard.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-function StepCard({ pictureSource, text }) {
+function StepCard({ pictureSource, alt, text }) {
     return (
         <Card className="text-center" style={{ width: "14rem" }}>
-            <Card.Img variant="top" src={pictureSource}></Card.Img>
+            <Card.Img variant="top" src={pictureSource} alt={alt}></Card.Img>
             <Card.Body>
                 <Card.Title>{text}</Card.Title>
             </Card.Body>
